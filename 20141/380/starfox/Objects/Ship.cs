601d8bb984fd7eca1914d93efaa35f23ae783406
abbogart <abbogart@bffe40b4-7709-9642-9d0b-c988e0290e46>
2014-04-04 00:22:00 +0000

    Reverting Jean's commit to rotate the ship.

--- 20141/380/starfox/Objects/Ship.cs
+++ 20141/380/starfox/Objects/Ship.cs
@@ -18,8 +18,6 @@ namespace itp380.Objects
 
         public Matrix RotationMatrix = Matrix.Identity;
 
-       
-
         private float rotation1;
         public float Rotation2
         {
@@ -49,16 +47,14 @@ namespace itp380.Objects
             //m_ModelName = "Miner/Miner";
             m_ModelName = "ship1";
             Scale = 0.4f;
-            m_Rotation = Quaternion.CreateFromAxisAngle(new Vector3(1.0f, -0.2f, 0.2f), 1.57f);
-            Angle = 1.57f;
         }
 
         public override void Update(float fDeltaTime)
         {
             base.Update(fDeltaTime);
-            //Rotation = Quaternion.CreateFromAxisAngle(new Vector3(0,0,1), Angle);
+            Rotation = Quaternion.CreateFromAxisAngle(new Vector3(0,0,1), Angle);
             Angle -= InputManager.Get().LeftThumbstick.X * .10f;
-            
+
             shipVelocity += Forward * .1f * InputManager.Get().RightTrigger;
             shipVelocity -= Forward * .1f * InputManager.Get().LeftTrigger;
 
