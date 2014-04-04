70f70cfb282599abcdb18de0f3b80d91ef7cf5b8
canellas <canellas@bffe40b4-7709-9642-9d0b-c988e0290e46>
2014-04-04 01:18:03 +0000

    Added new grassy floor object/texture. Got Textures working!

---
+++ 20141/380/starfox/Objects/grassfloor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace itp380.Objects
+{
+    public class grassfloor : GameObject
+    {
+        public grassfloor(Game game) :
+            base(game)
+        {
+            m_ModelName = "grassy_floor";
+            Scale = 3f;
+        }
+
+
+    }
+}
