daf3dfe60e9560a69e6ae2518b032f99bb73fc4f
piontkow <piontkow@bffe40b4-7709-9642-9d0b-c988e0290e46>
2014-04-09 01:18:06 +0000

    Projectiles as gamestate list instead of member of player.

--- 20141/380/starfox/Models/Player.cs
+++ 20141/380/starfox/Models/Player.cs
@@ -19,18 +19,11 @@ namespace itp380.Models
         {
             get { return m_Ship; }
         }
-
-        List<Objects.Projectile> m_Projectiles;
-        public List<Objects.Projectile> Projectiles
-        {
-            get { return m_Projectiles; }
-        }
                 
         public Player(Game game)
         {
             m_Health = 100;
             m_Ship = new Objects.Ship(game);
-            m_Projectiles = new List<Objects.Projectile>();
             GameState.Get().SpawnGameObject(m_Ship);
         }
     }
