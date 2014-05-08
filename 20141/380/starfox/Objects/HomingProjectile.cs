8982ea4805965b587ae4d9bd93f3f3a4e1bc17b8
duvoisin <duvoisin@bffe40b4-7709-9642-9d0b-c988e0290e46>
2014-05-08 09:57:50 +0000

    Homing missiles work.

--- 20141/380/starfox/Objects/HomingProjectile.cs
+++ 20141/380/starfox/Objects/HomingProjectile.cs
@@ -8,6 +8,9 @@ namespace itp380.Objects
 {
     class HomingProjectile : Projectile
     {
+        public float timePassed;
+        public float speedFactor;
+        public Vector3 sourceLocation;
         public Ship projectileTarget;
 
         public HomingProjectile(Game game, Ship projectileOwner, Ship projectileTarget) :
@@ -15,10 +18,19 @@ namespace itp380.Objects
         {
             m_ModelName = "homing_bullet1";
             this.projectileTarget = projectileTarget;
+            sourceLocation = projectileOwner.Position;
+            speedFactor = 1.0f;
+        }
+
+        public override void Update(float fDeltaTime)
+        {
             if (projectileTarget != null)
             {
-
+                timePassed += fDeltaTime;
+                Position = Vector3.Lerp(sourceLocation, projectileTarget.Position, timePassed * speedFactor);
             }
+
+            base.Update(fDeltaTime);
         }
     }
 }
